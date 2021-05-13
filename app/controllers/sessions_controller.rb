5064cbc5e62f3f780ce7c2bc923dc71e4c9263cf
Justine Saupin <sau.justine@gmail.com>
2021-05-13 14:38:32 +0200

    controller sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+def index
+    @sessions = Session.all
+  end
+
+  def show
+
+    @session = Session.find(params[:id])
+    @user = current_user
+  end
+
+  def new
+    @sessions = Session.new
+  end
+
+  def create
+    @session = Session.new(pp_params)
+    @session.user = current_user
+    @session.save
+    redirect_to profile_path
+  end
+
+  private
+
+  def pp_params
+    params.require(:session).permit(:title, :spot, :description, :date)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "cleaning DB..."
+Session.destroy_all
+User.destroy_all
+
+puts "creating users..."
+justine = User.new(email: 'juju@mail.com', password: '123321')
+# file = File.open(Rails.root.join('db/seeds/img_users/justine.jpg'))
+# justine.picture.attach(io: file, filename: 'justine.jpg', content_type: 'image/jpeg')
+justine.save!
+
+# pierre = User.new(email: 'pierren@mail.com', password: '123456')
+# # file = File.open(Rails.root.join('db/seeds/img_users/pierren.jpg'))
+# # pierre.picture.attach(io: file, filename: 'pierren.jpg', content_type: 'image/jpeg')
+# pierre.save!
+
+
+puts "creating sessions..."
+sauveterre = Session.new(title: "sauveterre on fire", date: '2021/08/10', spot: "sauveterre", description: 'Glassy, 1.5m aux séries, top !', user:justine)
+sauveterre.save!
+
+penhors = Session.new(title: "penhors on fire", date: '2021/08/10', spot: "penhors", description: 'Glassy, 1.5m aux séries, top !', user:justine)
+penhors.save!
+
+puts "finished !!!"
