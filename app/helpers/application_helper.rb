34f3a0ec2a8d3949bec9bba9f9f90f5829107620
Justine Saupin <sau.justine@gmail.com>
2021-05-21 20:57:58 +0200

    turbolinks fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,20 @@ module ApplicationHelper
   def tide(surf_session_tide)
     case surf_session_tide
     when "high-tide"
-      image_tag 'high-tide.svg'
+      image_tag 'high-tide.svg', width: 50, height: 50
     when "mid-tide"
-      image_tag 'mid-tide.svg'
+      image_tag 'mid-tide.svg', width: 50, height: 50
     when "low-tide"
-      image_tag "low-tide.svg"
+      image_tag "low-tide.svg", width: 50, height: 50
     end
   end
 
   def spot_type(surf_session_spot_type)
     case surf_session_spot_type
     when "reef"
-      image_tag 'reef.svg'
+      image_tag 'reef.svg', width: 50, height: 50
     when "beach"
-      image_tag 'beach.svg'
+      image_tag 'beach.svg', width: 50, height: 50
     end
   end
 end
