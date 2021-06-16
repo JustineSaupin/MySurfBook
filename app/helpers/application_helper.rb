59d0f660479dd96b6756f23474b0d6a5df5b214e
Justine Saupin <sau.justine@gmail.com>
2021-06-16 11:55:07 +0200

    change icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,20 @@ module ApplicationHelper
   def tide(surf_session_tide)
     case surf_session_tide
     when "high-tide"
-      image_tag 'high-tide.svg', width: 50, height: 50
+      image_tag 'high-tide.png', width: 35
     when "mid-tide"
-      image_tag 'mid-tide.svg', width: 50, height: 50
+      image_tag 'mid-tide.png', width: 35
     when "low-tide"
-      image_tag "low-tide.svg", width: 50, height: 50
+      image_tag "low-tide.png", width: 35
     end
   end
 
   def spot_type(surf_session_spot_type)
     case surf_session_spot_type
     when "reef"
-      image_tag 'reef.svg', width: 50, height: 50
+      image_tag 'reef.png', width: 35
     when "beach"
-      image_tag 'beach.svg', width: 50, height: 50
+      image_tag 'beach.png', width: 35
     end
   end
 end
