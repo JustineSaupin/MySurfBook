45dbd602a89f6de792b465d67f70dbcfb5f8edad
Justine Saupin <sau.justine@gmail.com>
2021-06-26 15:37:15 +0200

    cloudinary credentials

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 return if ENV["ASSETS_PRECOMPILE"].present?
 Cloudinary.config do |config|
-  config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
-  config.api_key = Rails.application.credentials.cloudinary[:api_key]
-  config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
+  config.cloud_name = Rails.application.credentials.cloudinary['dtwvios6f']
+  config.api_key = Rails.application.credentials.cloudinary['349753962719994']
+  config.api_secret = Rails.application.credentials.cloudinary['TptQRG55JlnnbIXbOyqI3rfsxD0']
   config.secure = true
 end
