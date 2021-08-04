b0ec88d36a6c0cca17626e0791c67c2a5719057c
Justine Saupin <sau.justine@gmail.com>
2021-08-04 16:37:25 +0200

    navbar modification

--- app/javascript/plugins/navbar.js
+++ app/javascript/plugins/navbar.js
@@ -3,8 +3,8 @@
 
     function showMenu() {
       console.log("menu log out");
-      document.querySelector('.show-menu').innerHTML =
-      `<%= link_to "Log out", destroy_user_session_path, method: :delete  %>`;
+      show = document.querySelector('.show-menu');
+      display(show);
   };
 
   menu.addEventListener('click', showMenu);
@@ -13,15 +13,9 @@
 
   function shareMenu() {
     console.log("menu share");
-    document.querySelector('.share-menu').innerHTML =
-    `<div class="share">
-      <div class="fb-share-button" data-href= "<%= request.url  %>" data-layout="button" data-size="large">
-        <a target="_blank"
-        href="https://www.facebook.com/sharer/sharer.php?u=<%=request.url%>%2F&amp;src=sdkpreparse"
-        class="fb-xfbml-parse-ignore">Share on FB</a>
-      </div>
-      <a class="WA" href="whatsapp://send?text=<%= request.url %>">WhatsApp</a>
-    </div>`;
+    shareOption = document.querySelector('.share-menu');
+    display(shareOption);
+
   };
 
   share.addEventListener('click', shareMenu);
