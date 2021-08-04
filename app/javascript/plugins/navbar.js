fd38d7f1c7b0ca17c88e9daaf14d37988fc41763
Justine Saupin <sau.justine@gmail.com>
2021-08-04 16:58:57 +0200

    pushing without nav option cause it s not fixed

--- app/javascript/plugins/navbar.js
+++ app/javascript/plugins/navbar.js
@@ -1,21 +1,23 @@
 
- let menu = document.querySelector('.nav-menu');
+ // let menu = document.querySelector('.nav-menu');
 
-    function showMenu() {
-      console.log("menu log out");
-      show = document.querySelector('.show-menu');
-      display(show);
-  };
+ //    function showMenu() {
+ //      console.log("menu log out");
+ //      show = document.querySelector('.show-menu');
+ //      display(show);
+ //  };
+ //  menu.addEventListener('click', showMenu);
 
-  menu.addEventListener('click', showMenu);
 
- let share = document.querySelector('.nav-share');
 
-  function shareMenu() {
-    console.log("menu share");
-    shareOption = document.querySelector('.share-menu');
-    display(shareOption);
 
-  };
+ // let share = document.querySelector('.nav-share');
 
-  share.addEventListener('click', shareMenu);
+ //  function shareMenu() {
+ //    console.log("menu share");
+ //    shareOption = document.querySelector('.share-menu');
+ //    display(shareOption);
+
+ //  };
+
+ //  share.addEventListener('click', shareMenu);
